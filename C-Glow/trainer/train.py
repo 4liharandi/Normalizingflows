1179a375d6f36eb2206be6976dc469800ccfb51f
Ali Aghababaei <71187157+4liharandi@users.noreply.github.com>
2022-01-31 00:03:40 +0330

    Update train.py

--- C-Glow/trainer/train.py
+++ C-Glow/trainer/train.py
@@ -7,6 +7,7 @@ import helper
 import os
 from .loss import *
 from torchvision.utils import save_image
+from torch.utils.data import DataLoader
 
 def init_train_configs(args):
     train_configs = {'reg_factor': args.reg_factor}  # lambda
@@ -46,9 +47,59 @@ def compute_peak_snr(target, sample):
     target, sample = target.detach().cpu().numpy(), sample.detach().cpu().numpy()
     return 20 * np.log10(np.max(target)) - 10 * np.log10(MSE(target, sample))
 
+def data_normalization(x):
+    
+    #x = x.astype('float32')
+    x = x - x.min()
+    x /= (x.max())
+    
+    return x
+
 def train(args, params, train_configs, model, optimizer, current_lr, resume=False, last_optim_step=0):
     # getting data loaders
-    train_loader, val_loader = data_handler.init_data_loaders(args, params)
+
+    data = np.load('/raid/Amir/Projects/datasets/CT_dataset/horizontal_snr25.0.npz')
+
+    train_images = data['x_train']
+    test_images = data['x_test']
+    
+    y_train = data['y_train']
+    y_test = data['y_test']
+
+    #train_images = np.load('/raid/Amir/Projects/datasets/CT_dataset/train_gt_horizontal_snr40.npy')
+    #print(np.shape(train_images))
+    train_images = data_normalization(train_images)
+    train_images = torch.from_numpy(train_images).permute(0, 3, 1, 2).cpu().detach().numpy()
+    #print(np.shape(train_images))
+    train_images = torch.Tensor(train_images)
+
+    #y_train = np.load('/raid/Amir/Projects/datasets/CT_dataset/train_fbp_horizontal_snr40.npy')
+    #print(np.shape(y_train))
+    y_train = data_normalization(y_train)
+    y_train = torch.from_numpy(y_train).permute(0, 3, 1, 2).cpu().detach().numpy()
+    y_train = torch.Tensor(y_train)
+    #test_images = np.load('/raid/Amir/Projects/datasets/CT_dataset/test_gt_horizontal_snr40.npy')
+    test_images = data_normalization(test_images)
+    test_images = torch.from_numpy(test_images).permute(0, 3, 1, 2).cpu().detach().numpy()
+    test_images = torch.Tensor(test_images)
+    #y_test = np.load('/raid/Amir/Projects/datasets/CT_dataset/test_fbp_horizontal_snr40.npy')
+    y_test = data_normalization(y_test)
+    y_test = torch.from_numpy(y_test).permute(0, 3, 1, 2).cpu().detach().numpy()
+    y_test = torch.Tensor(y_test)
+
+    train_dataset = torch.utils.data.TensorDataset(train_images,y_train)
+    test_dataset = torch.utils.data.TensorDataset(test_images,y_test)
+
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=4,
+                              pin_memory=True, drop_last=True)
+    val_loader = DataLoader(test_dataset, batch_size=256, shuffle=False, num_workers=4,
+                             pin_memory=True, drop_last=True)
+
+
+
+
+    #train_loader, val_loader = data_handler.init_data_loaders(args, params)
+    
     val_labels, val_imgs = next(iter(val_loader))
     val_labels, val_imgs = val_labels.cuda(), val_imgs.cuda()
     # adjusting optim step
@@ -67,7 +118,7 @@ def train(args, params, train_configs, model, optimizer, current_lr, resume=Fals
     while optim_step < max_optim_steps:
         # after each epoch, adjust learning rate accordingly
         current_lr, current_epoch = adjust_lr(current_lr, initial_lr=params['lr'], step=optim_step, epoch_steps=len(train_loader))  # now only supports with batch size 1
-        if current_epoch > 200:
+        if current_epoch > 300:
             exit(0)
         for param_group in optimizer.param_groups:
             param_group['lr'] = current_lr
@@ -108,7 +159,7 @@ def train(args, params, train_configs, model, optimizer, current_lr, resume=Fals
                 mean_nll = nll_sum / optims_in_epoch
                 nll_sum = 0
                 optims_in_epoch = 0
-                if (current_epoch % 5 == 0) or current_lr == 0 and not sampled:
+                if (current_epoch % 10 == 0) or current_lr == 0 and not sampled:
                     checkpoints_path = paths['checkpoints_path']
                     helper.make_dir_if_not_exists(checkpoints_path)
                     helper.save_checkpoint(checkpoints_path, optim_step, model, optimizer, loss, current_lr, epoch=True,
@@ -121,6 +172,11 @@ def train(args, params, train_configs, model, optimizer, current_lr, resume=Fals
                     PSNR = compute_peak_snr(val_labels, sampled_images)
                     print_info(log_file, current_epoch, optim_step, elapsed, mean_nll, current_lr, PSNR, log_info=True)
                     print(f'\nIn [train]: Sample saved at iteration {optim_step} to: \n"{samples_path}"\n')
+                    exp_dir = helper.compute_paths(args, params)['exp_base']
+                    log = open(os.path.join(exp_dir, 'psnr.log'), 'a')
+                    sample_from( exp_dir, log, current_epoch,10,26, model,val_imgs, val_labels, mean_samples=25)
+                    log.close()
+
                 else:
                     print_info(log_file, current_epoch, optim_step, elapsed, mean_nll, current_lr)
 
@@ -132,48 +188,84 @@ def train(args, params, train_configs, model, optimizer, current_lr, resume=Fals
                 sys.exit(0)
 
 def sample(args, params, model, optimizer):
-    train_loader, val_loader = data_handler.init_data_loaders(args, params)
+    
+    #train_loader1, val_loader1 = data_handler.init_data_loaders(args, params)
+    #val_labels1, val_imgs1 = next(iter(val_loader1))
+
+    #print(torch.min(val_labels1[1,:,:,1]))
+
+    
+    in_data = np.load('/raid/Amir/Projects/conditional_Trumpet/datasets/measure_de.npy')
+    in_data =  torch.from_numpy(in_data).permute(0, 3, 1, 2).cpu().detach().numpy()
+    in_data = torch.Tensor(in_data)
+    out_data = np.load('/raid/Amir/Projects/conditional_Trumpet/datasets/gt_de.npy')
+    out_data =  torch.from_numpy(out_data).permute(0, 3, 1, 2).cpu().detach().numpy()
+    out_data = torch.Tensor(out_data)
+
+    test_dataset = torch.utils.data.TensorDataset(out_data, in_data)
+
+    val_loader = DataLoader(test_dataset, batch_size=256, shuffle=False, num_workers=4,
+        pin_memory=True, drop_last=True)
+
     val_labels, val_imgs = next(iter(val_loader))
+    val_labels = torch.div(val_labels, 255)
+    val_imgs = torch.div(val_imgs, 255)
+
+
+    #print(torch.min(val_labels[1,:,:,1]))
+
+
     val_labels, val_imgs = val_labels.cuda(), val_imgs.cuda()
     exp_dir = helper.compute_paths(args, params)['exp_base']
     checkpoint_dir = os.path.join(exp_dir, 'checkpoints')
-    for e in range(5, 201, 5):
+    for e in range(200, 201, 10):
         log = open(os.path.join(exp_dir, 'psnr.log'), 'a')
         cp = os.path.join(checkpoint_dir, f'epoch={e}.pt')
-        sample_from(cp, exp_dir, log, e, 10, 8, model, val_imgs, val_labels, optimizer,  mean_samples=50)
+        sample_from(cp, exp_dir, log, e, 10, 26, model, val_imgs, val_labels, optimizer,  mean_samples=25)
         log.close()
         print(f'epoch {e} generated')
 
-def sample_from(path, img_dir,  log, epoch, num_show, num_samples, model, val_imgs, val_labels, optimizer,  mean_samples=50):
-    checkpoint = torch.load(path, map_location=device)
-    print(f'loaded epoch {epoch}')
-    model.load_state_dict(checkpoint['model_state_dict'])
+def sample_from(img_dir, log, epoch,num_show, num_samples, model, val_imgs, val_labels , mean_samples=25):
+	
+	#checkpoint = torch.load(path,map_location = device)
+	#print(f'loaded epoch {epoch}')
+	#model.load_state_dict(checkpoint['model_state_dict'])
 
-    if optimizer is not None:
-        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-    whitespace = torch.ones((3, num_show * 64, 10)).cuda()
-    first = True
-    with torch.no_grad():
-        mean = torch.zeros((256, 3, 64, 64)).cuda()
-        for j in range(mean_samples):
-            sample, _ = model(x=val_imgs, reverse=True, eps_std=0.2)
-            if first:
-                if j == 0:
-                    samples = stack_vertically(sample[0:num_show], num_show)
-                else:
-                    if j <= num_samples: samples = torch.cat((samples, stack_vertically(sample[0:num_show], num_show)),
-                                                             2)
-            mean = torch.add(mean, sample)
-        mean = torch.div(mean, mean_samples)
-        if first:
-            targets = stack_vertically(val_labels[0:num_show], num_show)
-            unres_images = stack_vertically(val_imgs[0:num_show], num_show)
-            means = stack_vertically(mean[0:num_show], num_show)
-            img = torch.cat((targets.cuda(), whitespace.cuda(), unres_images.cuda(), whitespace.cuda(), means.cuda(),
-                             whitespace.cuda(), samples.cuda()), 2)
-        sampled_snr = compute_peak_snr(mean, val_labels)
-        log.write(f'{epoch} \t{sampled_snr}\n')
-        save_image(img, os.path.join(img_dir, f'epoch_{epoch}.png'))
+	whitespace = torch.ones((1, num_show * 64, 10)).cuda()
+
+	first = True
+
+	with torch.no_grad():
+		mean = torch.zeros((256, 1, 64, 64)).cuda()
+		for j in range(mean_samples):
+			sample, _ = model(x=val_imgs, reverse=True, eps_std=0.2)
+
+			if first:
+				if j == 0:
+
+					samples = stack_vertically(sample[0:num_show], num_show)
+				else:
+
+					if j <= num_samples: samples = torch.cat((samples, stack_vertically(sample[0:num_show], num_show)),2)
+
+			mean = torch.add(mean, sample)
+		mean = torch.div(mean, mean_samples)
+		if first:
+			targets = stack_vertically(val_labels[0:num_show], num_show)
+			unres_images = stack_vertically(val_imgs[0:num_show], num_show)
+			means = stack_vertically(mean[0:num_show], num_show)
+			img = torch.cat((targets.cuda(), whitespace.cuda(), unres_images.cuda(), whitespace.cuda(), means.cuda(),whitespace.cuda(), samples.cuda()), 2)
+		sampled_snr = compute_peak_snr(val_labels[5:num_show], mean[5:num_show])
+		log.write(f'{epoch} \t{sampled_snr}\n')
+		
+		sam25_dir = os.path.join(img_dir, 'ehsan')
+		if not os.path.exists(sam25_dir):
+			os.mkdir(sam25_dir)
+		#np.save(os.path.join(sam25_dir, 'final.npy'), final)
+		save_image(img, os.path.join(sam25_dir, f'epoch_{epoch}.png'))
+
+        #np.save(os.path.join(sam25_dir, f'mean_epoch_{epoch}.npy'), mean_pt)
+        #np.save(os.path.join(sam25_dir, f'target_epoch_{epoch}.npy'), target_pt)
 
 def stack_vertically(imgs, num_show):
     for i in range(num_show):
@@ -182,4 +274,3 @@ def stack_vertically(imgs, num_show):
         else:
             stack = torch.cat((stack, imgs[i]), 1)
     return stack
-
